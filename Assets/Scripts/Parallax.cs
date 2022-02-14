6cdfe03a5f132f63a27500b75c5946e114a485fe
jaspercrew <justinph2013@gmail.com>
2022-02-14 01:48:44 -0800

    sc

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -7,7 +7,7 @@ public class Parallax : MonoBehaviour
     private float length, startpos;
     [SerializeField]
     public GameObject cam;
-    public float parallexEffect;
+    public float parallaxEffect;
     void Start()
     {
         startpos = transform.position.x;
@@ -15,8 +15,8 @@ public class Parallax : MonoBehaviour
     }
     void Update()
     {
-        float temp = (cam.transform.position.x * (1 - parallexEffect));
-        float dist = (cam.transform.position.x * parallexEffect);
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
         if (temp > startpos + length) startpos += length;
         else if (temp < startpos - length) startpos -= length;

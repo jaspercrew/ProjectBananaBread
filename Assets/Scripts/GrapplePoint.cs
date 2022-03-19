ffa397fc3e21df578811a657533bb3bb1192f6be
burgerhex <aviv.shai@gmail.com>
2022-03-18 21:27:24 -0700

    Grapple point

--- Assets/Scripts/GrapplePoint.cs
+++ Assets/Scripts/GrapplePoint.cs
@@ -1,34 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Debug = System.Diagnostics.Debug;
 
 public class GrapplePoint : MonoBehaviour
 {
-    private const float hoverRange = 3f;
-    public static HashSet<GameObject> availablePoints;
+    private const float HoverRange = 3f;
+    public static GrapplePoint CurrentGrapplePoint;
     private SpriteRenderer spriteRenderer;
+    private new Camera camera;
 
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        camera = Camera.main;
     }
     
     private void Update()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if ((mousePos - transform.position).magnitude < hoverRange) //in range of mouse
+        Vector3 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+
+        float thisDist = Vector3.Distance(mousePos, transform.position);
+        float currDist = (CurrentGrapplePoint is null) ? float.MaxValue :
+            Vector3.Distance(mousePos, CurrentGrapplePoint.transform.position);
+        
+        if (thisDist < HoverRange && thisDist < currDist)
         {
-            //do stuff
-            
-            
-            if (!availablePoints.Contains(gameObject))
-            {
-                availablePoints.Add(gameObject);
-            }
-        }
-        else if (availablePoints.Contains(gameObject))
+            CurrentGrapplePoint = this;
+        } 
+        else if (currDist >= HoverRange)
         {
-            availablePoints.Remove(gameObject);
+            CurrentGrapplePoint = null;
         }
     }
 

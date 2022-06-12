39ce885fa3bca1e52deb6a331828b1017e57d0fa
burgerhex <aviv.shai@gmail.com>
2022-06-12 05:40:09 -0700

    Parallax change

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -1,42 +1,25 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private float length, startpos;
+    public Camera cam;
+    [Header("0 = follows camera exactly, 1 = does not move at all")]
     [SerializeField]
-    public GameObject cam;
+    public float parallaxEffectX;
 
-    public float parallaxEffect = .3f;
+    private Vector3 lastCameraPos;
 
-    void Start()
+    private void Start()
     {
-        startpos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
-        StartCoroutine(LateStart());
-        
+        cam = Camera.main;
+        lastCameraPos = cam.transform.position;
     }
-
-    private IEnumerator LateStart()
+    
+    private void FixedUpdate()
     {
-        yield return new WaitForEndOfFrame();
-        if (cam == null)
-        {
-            cam = GameObject.FindWithTag("MainCamera");
-        }
-    }
-    void Update()
-    {
-        if (cam == null)
-            cam = GameObject.FindWithTag("MainCamera");
-        
-        //transform.position = new Vector3(transform.position.x, cam.transform.position.y, transform.position.z);
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
-        if (temp > startpos + length) startpos += length;
-        else if (temp < startpos - length) startpos -= length;
+        Vector3 pos = cam.transform.position;
+        float dx = pos.x - lastCameraPos.x;
+        transform.position += dx * (1 - parallaxEffectX) * Vector3.right;
+        lastCameraPos = pos;
     }
 }

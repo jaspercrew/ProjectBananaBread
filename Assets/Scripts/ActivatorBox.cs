9e33af03417106c0d503e5d141b19ce5535229f0
jaspercrew <47955720+jaspercrew@users.noreply.github.com>
2022-06-14 18:29:28 -0700

    puzzle sprites and puzzle hitbox adjustments:

--- Assets/Scripts/ActivatorBox.cs
+++ Assets/Scripts/ActivatorBox.cs
@@ -6,16 +6,22 @@ public class ActivatorBox : Entity
 {
     private Collider2D collider2D;
     private Rigidbody2D rigidbody2D;
+    private SpriteRenderer spriteRenderer;
+    public Sprite offSprite;
+    public Sprite onSprite; 
     
     // Start is called before the first frame update
     void Start()
     {
         collider2D = GetComponent<Collider2D>();
         rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = offSprite;
     }
 
     public void Lock()
     {
+        GetComponent<SpriteRenderer>().sprite = onSprite;
         rigidbody2D.constraints = RigidbodyConstraints2D.FreezeAll;
     }
 
--- Assets/Scripts/ActivatorHolder.cs
+++ Assets/Scripts/ActivatorHolder.cs
@@ -7,13 +7,17 @@ public class ActivatorHolder : ActivatorTrigger
 {
     
     private Collider2D Collider2D;
+    private SpriteRenderer spriteRenderer;
     public float yLockOffset = .5f;
+    public Sprite offSprite;
+    public Sprite onSprite;
     
     // Start is called before the first frame update
     void Start()
     {
         Collider2D = GetComponent<Collider2D>();
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = offSprite;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -21,6 +25,7 @@ public class ActivatorHolder : ActivatorTrigger
         ActivatorBox activatorBox = other.gameObject.GetComponent<ActivatorBox>();
         if (activatorBox != null)
         {
+            spriteRenderer.sprite = onSprite;
             Activate();
             activatorBox.Lock();
             other.transform.position = new Vector3(transform.position.x, transform.position.y + yLockOffset, transform.position.z);

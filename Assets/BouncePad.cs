927af8bc87bdfbc0b88cde748c3c353fca31df83
jaspercrew <47955720+jaspercrew@users.noreply.github.com>
2022-06-16 22:45:53 -0700

    scene work, puzzle mechs

--- Assets/BouncePad.cs
+++ Assets/BouncePad.cs
@@ -7,6 +7,7 @@ using UnityEngine.Serialization;
 public class BouncePad : ActivatedEntity
 {
     public float bounceVelocity;
+    private const float charMultiplier = 1.2f;
     public bool isInverted;
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -21,11 +22,12 @@ public class BouncePad : ActivatedEntity
             //print("force added");
             if (other.gameObject.GetComponent<CharController>() == null)
             {
-                rb.velocity = new Vector2(0, rb.velocity.y + (isInverted ? -bounceVelocity : bounceVelocity));
+                rb.velocity = new Vector2(0, isInverted ? -bounceVelocity : bounceVelocity);
             }
             else
             {
-                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + (isInverted ? -bounceVelocity : bounceVelocity));
+                rb.velocity = new Vector2(rb.velocity.x, isInverted ? 
+                    (-bounceVelocity * charMultiplier) : (bounceVelocity * charMultiplier));
             }
         }
     }

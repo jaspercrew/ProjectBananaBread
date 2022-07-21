739872fc6bda421645e6e3be6417cc0fff5a3dc3
jaspercrew <47955720+jaspercrew@users.noreply.github.com>
2022-07-20 17:39:19 -0700

    more asset deletion, windburst visuals added

--- Assets/WindBurst.cs
+++ Assets/WindBurst.cs
@@ -8,10 +8,13 @@ public class WindBurst : BeatEntity
     public Vector2 direction;
     public float windVelocity;
     private bool playerInRange;
+
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     protected override void BeatAction()
@@ -20,8 +23,31 @@ public class WindBurst : BeatEntity
         {
             //Vector2 vel = CharController.Instance.GetComponent<Rigidbody2D>().velocity;
             CharController.Instance.GetComponent<Rigidbody2D>().velocity = windVelocity * direction.normalized;
-
         }
+
+        StartCoroutine(FlashCoroutine());
+
+    }
+
+    private IEnumerator FlashCoroutine()
+    {
+        Color initialColor = spriteRenderer.color;
+        
+        Color fullAlpha = initialColor;
+        fullAlpha.a = 1;
+        spriteRenderer.color = fullAlpha;
+        
+        float elapsedTime = 0f;
+        float flashTime = 1.5f; 
+        while (elapsedTime < flashTime)
+        {
+            spriteRenderer.color = Color.Lerp(fullAlpha, initialColor, elapsedTime / flashTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        spriteRenderer.color = initialColor;
+        yield return null;
     }
 
     private void OnTriggerStay2D(Collider2D other)

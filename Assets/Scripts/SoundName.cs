5a5452fa9b712b82565e9a313d16b89eb08bab91
burgerhex <aviv.shai@gmail.com>
2022-09-16 13:28:08 -0700

    Fix synth looping issue (Tyler diff)

--- Assets/Scripts/SoundName.cs
+++ Assets/Scripts/SoundName.cs
@@ -2,5 +2,7 @@ using System.Diagnostics.CodeAnalysis;
 
 [SuppressMessage("ReSharper", "InconsistentNaming")]
 public enum SoundName {
-    None, Hit, Banger_A, Banger_B, Something_A, Something_B, Dunno_A, Dunno_B, WTS, Mario, Snare, Future, Synth_Back, Synth_Lead, Synth_Perc, Menu, ExtendedSnare
+    None, Hit, Banger_A, Banger_B, Something_A, Something_B, Dunno_A, Dunno_B, WTS, Mario, Snare, 
+    Future, Synth_Back, Synth_Back_wav, Synth_Lead, Synth_Perc, Menu, ExtendedSnare,
+    synth_back_no_clip
 }

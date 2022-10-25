ae3acff41d4519122929dca9ac75f6099eff87b8
Justin Hu <juphu@ucsc.edu>
2022-10-24 17:59:25 -0700

    scene 1 rework

--- Assets/Scripts/SoundName.cs
+++ Assets/Scripts/SoundName.cs
@@ -4,5 +4,6 @@ using System.Diagnostics.CodeAnalysis;
 public enum SoundName {
     None, Hit, Banger_A, Banger_B, Something_A, Something_B, Dunno_A, Dunno_B, WTS, Mario, Snare, 
     Future, Synth_Back, Synth_Back_wav, Synth_Lead, Synth_Perc, Menu, ExtendedSnare,
-    synth_back_no_clip, Gravity
+    synth_back_no_clip, Gravity, Synth_BackDrum, Synth_Stab
+    
 }
